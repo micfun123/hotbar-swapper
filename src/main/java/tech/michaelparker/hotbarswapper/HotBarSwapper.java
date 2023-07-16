725eb3fdc91e0f4bc684e89a7ea3294d72ae705c
Michael Parker <michaelrbparker@protonmail.com>
2023-07-16 10:53:29 +0100

    fixed set up

--- src/main/java/tech/michaelparker/hotbarswapper/HotBarSwapper.java
+++ src/main/java/tech/michaelparker/hotbarswapper/HotBarSwapper.java
@@ -7,7 +7,7 @@ public final class HotBarSwapper extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("HotBarSwapper has been enabled!");
     }
 
     @Override
